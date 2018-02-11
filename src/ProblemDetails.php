bb2c2353e2d33b409b4992f27732af7357104ca7
Hunter Skrasek <hunterskrasek@me.com>
2018-02-11 12:46:19 -0600

    First pass at a ProblemDetails object, to convert validation errors into a ProblemDetails API response.
    Signed-off-by: Hunter Skrasek <hunter@perk.com>
    Signed-off-by: Hunter Skrasek <hunterskrasek@me.com>

---
+++ src/ProblemDetails.php
@@ -0,0 +1,88 @@
+<?php declare(strict_types=1);
+
+namespace HSkrasek\JSONSchema;
+
+final class ProblemDetails implements \JsonSerializable
+{
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var int
+     */
+    private $status;
+
+    /**
+     * @var string
+     */
+    private $detail;
+
+    /**
+     * @var array
+     */
+    private $extensions;
+
+    public function __construct(string $title, int $status, string $detail, array $extensions = [])
+    {
+        $this->title      = $title;
+        $this->status     = $status;
+        $this->detail     = $detail;
+        $this->extensions = $extensions;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTitle(): string
+    {
+        return $this->title;
+    }
+
+    /**
+     * @return int
+     */
+    public function getStatus(): int
+    {
+        return $this->status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDetail(): string
+    {
+        return $this->detail;
+    }
+
+    /**
+     * @return array
+     */
+    public function getExtensions(): array
+    {
+        return $this->extensions;
+    }
+
+    /**
+     * Specify data which should be serialized to JSON
+     *
+     * @link  http://php.net/manual/en/jsonserializable.jsonserialize.php
+     * @return mixed data which can be serialized by <b>json_encode</b>,
+     * which is a value of any type other than a resource.
+     * @since 5.4.0
+     */
+    public function jsonSerialize()
+    {
+        return $this->toArray();
+    }
+
+    public function toArray(): array
+    {
+        return [
+                'title'  => $this->title,
+                'detail' => $this->detail,
+                'status' => $this->status,
+            ] + $this->extensions;
+    }
+}
---
+++ src/Validator.php
@@ -0,0 +1,69 @@
+<?php declare(strict_types=1);
+
+namespace HSkrasek\JSONSchema;
+
+use League\JsonGuard\ValidationError;
+use League\JsonGuard\Validator as JsonGuardValidator;
+use Psr\Container\ContainerInterface;
+
+class Validator
+{
+    /**
+     * @var JsonGuardValidator
+     */
+    private $validator;
+
+    /**
+     * Validator constructor.
+     *
+     * @param mixed $data
+     * @param object $schema
+     * @param null|ContainerInterface $ruleSet
+     */
+    public function __construct($data, $schema, ?ContainerInterface $ruleSet = null)
+    {
+        $this->validator = new JsonGuardValidator($data, $schema, $ruleSet);
+    }
+
+    /**
+     * Determines if the data passes schema validation.
+     *
+     * @return bool
+     *
+     * @throws \League\JsonGuard\Exception\InvalidSchemaException
+     * @throws \League\JsonGuard\Exception\MaximumDepthExceededException
+     */
+    public function passes(): bool
+    {
+        return $this->validator->passes();
+    }
+
+    /**
+     * Determines if the data does not pass schema validation.
+     *
+     * @return bool
+     *
+     * @throws \League\JsonGuard\Exception\InvalidSchemaException
+     * @throws \League\JsonGuard\Exception\MaximumDepthExceededException
+     */
+    public function fails(): bool
+    {
+        return $this->validator->fails();
+    }
+
+    /**
+     * @return array|ValidationError[]
+     *
+     * @throws \League\JsonGuard\Exception\InvalidSchemaException
+     * @throws \League\JsonGuard\Exception\MaximumDepthExceededException
+     */
+    public function errors(): array
+    {
+        return $this->validator->errors();
+    }
+
+    public function getProblemDetails(): ProblemDetails
+    {
+        
+    }
+}
